8e5ac2bb50db9635d408365e29e56c338dd321d7
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-07-27 02:25:13 +0800

    Bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Flexphoenix.Mixfile do
 
   def project do
     [app: :flexphoenix,
-     version: "0.0.1",
+     version: "0.1.3",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,

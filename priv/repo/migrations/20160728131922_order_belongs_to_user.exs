71ab4b70a6cb3652a9aec40cfde46516f986aca7
Faruq Luqman <faruqluqman@hotmail.com>
2016-07-28 22:15:40 +0800

    Correction in migration file
    * Table "order" is undefined. Table "orders" is the corresponding table that is in Postgres.

--- priv/repo/migrations/20160728131922_order_belongs_to_user.exs
+++ priv/repo/migrations/20160728131922_order_belongs_to_user.exs
@@ -2,7 +2,7 @@ defmodule Flexphoenix.Repo.Migrations.OrderBelongsToUser do
   use Ecto.Migration
 
   def change do
-    alter table(:order) do
+    alter table(:orders) do
       add :user_id, references(:users)
     end
   end

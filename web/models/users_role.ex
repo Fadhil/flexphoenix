ef18411ed2c4d422c0172e708bc7832f6da4108e
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-07-30 20:11:07 +0800

    Add UsersRole.create_changeset
    * Checks if the UsersRole exists, if it does, returns a changeset using that users_role, otherwise returns a changeset that uses an empty UsersRole map

--- web/models/users_role.ex
+++ web/models/users_role.ex
@@ -1,5 +1,6 @@
 defmodule Flexphoenix.UsersRole do
   use Flexphoenix.Web, :model
+  import Ecto.Query, only: [from: 2]
 
   schema "users_roles" do
     belongs_to :user, Flexphoenix.User
@@ -21,5 +22,26 @@ defmodule Flexphoenix.UsersRole do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+  end
+
+  def create_changeset(model, repo, users_role_changes = %{
+      role_id: role_id, user_id: user_id, project_id: project_id
+    }) do
+
+    query = from ur in model,
+      where: ur.user_id == ^user_id and ur.role_id == ^role_id and ur.project_id == ^project_id
+
+    result =
+      case repo.all(query) do
+        # If we get no matches we build an empty map
+        [] -> %Flexphoenix.UsersRole{}
+        # If we get only the one matching row, we return that user role
+        [users_role|[]] -> users_role
+      end
+      # Get a changeset using either the empty map or the user role (
+      # which is returned by Repo.all(query))
+      |> changeset(users_role_changes)
+
+
   end
 end

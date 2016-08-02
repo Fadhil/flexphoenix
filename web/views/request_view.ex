9486e2fd219b564cb6ff0afc68f7ecd5b5b32263
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-08-02 12:56:54 +0800

    Add RequestView.asset_list
    * Returns a list of select choices for the dropdown, e.g.:
    [{"Assetname1", 1}, {"Assetname2",2}]

--- web/views/request_view.ex
+++ web/views/request_view.ex
@@ -1,5 +1,6 @@
 defmodule Flexphoenix.RequestView do
   use Flexphoenix.Web, :view
+  alias Flexphoenix.Repo
 
   def project_list(user) do
     all_projects(user)
@@ -7,6 +8,14 @@ defmodule Flexphoenix.RequestView do
     |> Enum.map(fn x -> {x.name, x.id} end)
   end
 
+  def asset_list(user, project_id) do
+    selected_project = all_projects(user)
+      |> Enum.find(fn project -> project.id == project_id end)
+      |> Repo.preload(:assets)
+
+    selected_project.assets
+    |> Enum.map(fn x -> {x.name, x.id} end)
+  end
 
   def all_projects(user) do
     user.attached_projects ++ user.projects

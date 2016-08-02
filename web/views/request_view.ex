76ac2318d1ff916064b64320224088f8ba1bd15b
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-08-02 12:30:34 +0800

    Show select list of projects user has access to
    * Create RequestView.project_select_list method

--- web/views/request_view.ex
+++ web/views/request_view.ex
@@ -1,3 +1,12 @@
 defmodule Flexphoenix.RequestView do
   use Flexphoenix.Web, :view
+
+  def project_select_list(user) do
+    attached_projects = user.attached_projects
+    own_projects = user.projects
+
+    own_projects ++ attached_projects
+    |> Enum.uniq
+    |> Enum.map(fn x -> {x.name, x.id} end)
+  end
 end

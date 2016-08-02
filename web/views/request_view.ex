6ddce68a4feb4a8527faef9644f8de3db1739ff0
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-08-02 12:44:35 +0800

    Simplify RequestView.project_list

--- web/views/request_view.ex
+++ web/views/request_view.ex
@@ -2,11 +2,13 @@ defmodule Flexphoenix.RequestView do
   use Flexphoenix.Web, :view
 
   def project_list(user) do
-    attached_projects = user.attached_projects
-    own_projects = user.projects
-
-    own_projects ++ attached_projects
+    all_projects(user)
     |> Enum.uniq
     |> Enum.map(fn x -> {x.name, x.id} end)
+  end
+
+
+  def all_projects(user) do
+    user.attached_projects ++ user.projects
   end
 end

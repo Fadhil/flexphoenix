4a4fc7bff491e68cf0f3c1501c2204210b253378
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-08-03 15:46:37 +0800

    Add default changeset for AssignedTechnician

--- web/models/assigned_technician.ex
+++ web/models/assigned_technician.ex
@@ -12,4 +12,19 @@ defmodule Flexphoenix.AssignedTechnician do
     has_many :requests, Flexphoenix.Request
     has_many :orders, Flexphoenix.Order
   end
+
+  @required_fields ~w(user_id)
+  @optional_fields ~w(request_id order_id)
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+
 end

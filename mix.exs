e4356649830e54ce36ae981bda6cd46e7b1e041d
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-08-06 22:24:49 +0800

    Bump version to 0.9.0
    Basic chat

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Flexphoenix.Mixfile do
 
   def project do
     [app: :flexphoenix,
-     version: "0.8.2",
+     version: "0.9.0",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,

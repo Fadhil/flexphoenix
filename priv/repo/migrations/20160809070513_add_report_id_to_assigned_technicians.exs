ca6e2d2d80425a3caf67e735c54a15ad753258c4
Faruq Luqman <faruqluqman@hotmail.com>
2016-08-09 15:10:15 +0800

    Add report_id to assigned_technicians

---
+++ priv/repo/migrations/20160809070513_add_report_id_to_assigned_technicians.exs
@@ -0,0 +1,11 @@
+defmodule Flexphoenix.Repo.Migrations.AddReportIdToAssignedTechnicians do
+  use Ecto.Migration
+
+  def change do
+    alter table(:assigned_technicians) do
+      add :report_id, references(:reports, on_delete: :nothing)
+    end
+
+    create index(:assigned_technicians, [:user_id, :report_id])
+  end
+end
--- web/models/assigned_technician.ex
+++ web/models/assigned_technician.ex
@@ -11,7 +11,7 @@ defmodule Flexphoenix.AssignedTechnician do
   end
 
   @required_fields ~w(user_id)
-  @optional_fields ~w(request_id order_id)
+  @optional_fields ~w(request_id order_id report_id)
 
   @doc """
   Creates a changeset based on the `model` and `params`.

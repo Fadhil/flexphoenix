69e3748c6fb83d1e7e8ab1dcf78b94e1d9b4e74a
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-08-11 22:33:08 +0800

    Import profile_image into OrderView

--- web/views/order_view.ex
+++ web/views/order_view.ex
@@ -1,5 +1,5 @@
 defmodule Flexphoenix.OrderView do
   use Flexphoenix.Web, :view
   import Flexphoenix.RequestView, only: [asset_name: 1, project_name: 1]
-  import Flexphoenix.LayoutView, only: [display_name: 1, image: 1]
+  import Flexphoenix.LayoutView, only: [display_name: 1, image: 1, profile_image: 1]
 end

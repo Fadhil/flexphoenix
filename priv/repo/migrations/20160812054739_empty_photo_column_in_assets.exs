39218b6f939212424b73ab300fec38a824afaa32
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-08-12 13:48:31 +0800

    Add migration to reinitialize photo column to null

---
+++ priv/repo/migrations/20160812054739_empty_photo_column_in_assets.exs
@@ -0,0 +1,14 @@
+defmodule Flexphoenix.Repo.Migrations.EmptyPhotoColumnInAssets do
+  use Ecto.Migration
+
+  def up do
+    alter table(:assets) do
+      remove :photo
+      add :photo, :string
+    end
+  end
+
+  def down do
+
+  end
+end

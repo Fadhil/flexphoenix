71957cffac0688d2b1d3d0cce03faef606f66432
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-02-14 23:01:29 +0800

    remove drop_index

--- priv/repo/migrations/20170214065619_remove_user_id_from_organisations.exs
+++ priv/repo/migrations/20170214065619_remove_user_id_from_organisations.exs
@@ -5,8 +5,6 @@ defmodule Flexcility.Repo.Migrations.RemoveUserIdFromOrganisations do
     alter table(:organisations) do
       remove :user_id
     end
-
-    drop_index(:organisations, [:user_id])
   end
 
   def down do

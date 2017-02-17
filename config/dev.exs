ed5e9242820ba7dfa5b544f3d8ce830aa52dabf3
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-02-18 02:51:12 +0800

    Allow dev server on local ip

--- config/dev.exs
+++ config/dev.exs
@@ -8,6 +8,7 @@ use Mix.Config
 # with brunch.io to recompile .js and .css sources.
 config :Flexcility, Flexcility.Endpoint,
   http: [port: 4000],
+	url: [host: "192.168.0.22", port: 4000 ],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,

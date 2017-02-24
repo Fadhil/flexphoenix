eb1c453ecea8f78b7fd9c1a63bba69cf6cc57176
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-02-24 21:37:03 +0800

    Fix wrongly changed "Project" references in older migrations

--- priv/repo/migrations/20170217205344_change_users_roles_project_id_to_site_id.exs
+++ priv/repo/migrations/20170217205344_change_users_roles_project_id_to_site_id.exs
@@ -2,6 +2,6 @@ defmodule Flexcility.Repo.Migrations.ChangeUsersRolesProjectIdToSiteId do
   use Ecto.Migration
 
   def change do
-    rename table(:users_roles), :site_id, to: :site_id
+    rename table(:users_roles), :project_id, to: :site_id
   end
 end

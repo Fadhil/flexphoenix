1a10591d5e91ca4eb33f9d91535e36f0a0e5e0d3
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-02-28 23:12:03 +0800

    Add new 404 template

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -1,9 +1,9 @@
 defmodule Flexcility.ErrorView do
   use Flexcility.Web, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
+  # def render("404.html", _assigns) do
+  #   "Page not found"
+  # end
 
   def render("500.html", _assigns) do
     "Server internal error"

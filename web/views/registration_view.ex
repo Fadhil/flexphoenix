e3ec3d6a1f87d5211d8eb37a3cb9582f63918245
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-03-02 00:32:25 +0800

    Add Registration#Invitation template

--- web/views/registration_view.ex
+++ web/views/registration_view.ex
@@ -1,3 +1,18 @@
 defmodule Flexcility.RegistrationView do
   use Flexcility.Web, :view
+
+  import Flexcility.LayoutView, only: [image: 1, thumb: 1]
+
+  def display_invited_role(role) do
+    case role.name do
+      "Helpdesk" ->
+        "As a Helpdesk User"
+      "Technician" ->
+        "As a Technician"
+      "Engineer" ->
+        "As an Engineer"
+      "Admin" ->
+        "As an Admin"
+    end
+  end
 end
--- web/views/subdomain/invitation_view.ex
+++ web/views/subdomain/invitation_view.ex
@@ -1,3 +1,5 @@
 defmodule Flexcility.Subdomain.InvitationView do
   use Flexcility.Web, :view
+
+  import Flexcility.LayoutView, only: [image: 1, thumb: 1]
 end

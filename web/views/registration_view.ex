e2470728b5e1f1afb106f911319110634de747ed
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-03-02 19:38:13 +0800

    Fix Registration form when no invitation
    * Remove RegistrationView.display_invited_role

--- web/views/registration_view.ex
+++ web/views/registration_view.ex
@@ -3,16 +3,4 @@ defmodule Flexcility.RegistrationView do
 
   import Flexcility.LayoutView, only: [image: 1, thumb: 1]
 
-  def display_invited_role(role) do
-    case role.name do
-      "Helpdesk" ->
-        "As a Helpdesk User"
-      "Technician" ->
-        "As a Technician"
-      "Engineer" ->
-        "As an Engineer"
-      "Admin" ->
-        "As an Admin"
-    end
-  end
 end
